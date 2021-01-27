c1f5f7e6bb4973e4ee4ac30ca4a12645fb6b6973
Odinaka Ezennia <Odi@Users-MacBook-Pro.local>
2021-01-27 03:01:59 +0100

    update backednUrl for login redirect

--- src/containers/LoginRedirect.js
+++ src/containers/LoginRedirect.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { useHistory, useLocation, useParams } from "react-router-dom";
+import { useHistory, useLocation, useParams } from 'react-router-dom';
 import Container from '../components/Container';
 import { getUser, setToken } from '../redux/actions';
 
 // const backendUrl = process.env.REACT_APP_BACKEND_URL;
-const backendUrl = 'https://cityshoppa.herokuapp.com';
+const backendUrl = 'https://api.cityshoppa.com';
 
-const LoginRedirect = (props) => {
+const LoginRedirect = props => {
   const [text, setText] = useState('Loading...');
   const location = useLocation();
   const params = useParams();
@@ -30,27 +30,33 @@ const LoginRedirect = (props) => {
         // localStorage.setItem('jwt', res.jwt);
         // localStorage.setItem('username', res.user.username);
 
-        props.setToken(res.jwt)
-        props.getUser()
-        
-        setText('You have been successfully logged in. You will be redirected in a few seconds...');
+        props.setToken(res.jwt);
+        props.getUser();
+
+        setText(
+          'You have been successfully logged in. You will be redirected in a few seconds...'
+        );
         setTimeout(() => history.push('/'), 3000); // Redirect to homepage after 3 sec
       })
       .catch(err => {
         console.log(err);
-        setText('An error occurred.')
+        setText('An error occurred.');
       });
-      // eslint-disable-next-line
+    // eslint-disable-next-line
   }, [history, location.search, params.provider]);
 
-  return <Container><p>{text}</p></Container>
+  return (
+    <Container>
+      <p>{text}</p>
+    </Container>
+  );
 };
 
 const mapDispatchToProps = dispatch => {
-    return {
-        setToken: (token) => dispatch(setToken(token)),
-        getUser: () => dispatch(getUser),
-    }
-}
+  return {
+    setToken: token => dispatch(setToken(token)),
+    getUser: () => dispatch(getUser)
+  };
+};
 
 export default connect(null, mapDispatchToProps)(LoginRedirect);
